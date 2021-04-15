0683a1d574e3ff121b6109c3a91b0bd8b7690eef
Nick <41241228+nitria@users.noreply.github.com>
2021-04-15 19:45:49 +0300

    commit

--- main.js
+++ main.js
@@ -17,3 +17,34 @@ $(function () {
     }
   });
 });
+
+/*SHOW FORMS*/
+const userBtnLogin = document.querySelector(".userbtnlogin");
+const userBtnSignup = document.querySelector(".userbtnsignup");
+const loginForm = document.querySelector(".form1");
+const signupForm = document.querySelector(".form2");
+const closeBtn = document.querySelectorAll(".close");
+const contactBtn = document.querySelector(".contactUs");
+const contactForm = document.querySelector(".contactForm");
+
+userBtnLogin.addEventListener("click", () => {
+  loginForm.classList.add("active");
+  signupForm.classList.remove("active");
+});
+
+userBtnSignup.addEventListener("click", () => {
+  signupForm.classList.add("active");
+  loginForm.classList.remove("active");
+});
+
+contactBtn.addEventListener("click", () => {
+  contactForm.classList.add("active");
+});
+
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    loginForm.classList.remove("active");
+    signupForm.classList.remove("active");
+    contactForm.classList.remove("active");
+  });
+});
